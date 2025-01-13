9aa28fbebb0f11cfb814c8162563e866566f1144
Valmir Júnior <61609240+Valmir-unicap@users.noreply.github.com>
2025-01-13 18:40:08 -0300

    Create webContentFolderName.java

---
+++ API para descobrir em qual pasta o conteúdo web está presente/webContentFolderName.java
@@ -0,0 +1,96 @@
+package br.com.notrelabs.tasy.authenticator.application;
+
+import com.liferay.dynamic.data.mapping.model.DDMStructure;
+import com.liferay.journal.model.JournalArticle;
+import com.liferay.journal.service.JournalArticleLocalServiceUtil;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.json.JSONException;
+import com.liferay.portal.kernel.json.JSONFactoryUtil;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.jaxrs.whiteboard.JaxrsWhiteboardConstants;
+
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Application;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Component(immediate = true, configurationPid = "br.com.notrelabs.tasy.authenticator.application.TasyConfiguration", property = {
+        JaxrsWhiteboardConstants.JAX_RS_APPLICATION_BASE + "=/tasy",
+        JaxrsWhiteboardConstants.JAX_RS_NAME + "=Tasy.Authenticator",
+        "auth.verifier.guest.allowed=true",
+        "liferay.access.control.disable=true"
+},
+        service = Application.class)
+public class webContentRestServiceApplication extends Application {
+
+    public Set<Object> getSingletons() {
+        return Collections.singleton(this);
+    }
+
+    @POST
+    @Path("/filtar-nome")
+    public Response filterWebContentFolderName(String payload) throws JSONException {
+
+        JSONObject object;
+        object = JSONFactoryUtil.createJSONObject(payload);
+
+        if (object == null || object.length() == 0) {
+            _log.error(object);
+            return Response.status(Response.Status.BAD_REQUEST)
+                    .entity("{\"error\":\"Bad Request: JSON vázio ou inválido.\"}").build();
+        }
+
+        try {
+
+            long groupId = Long.parseLong(object.getString("groupId"));
+            JournalArticle pontJornalArticle = null;
+
+            String arquivo = object.getString("arquivo");
+            String name = object.getString("name");
+
+            if (name.isEmpty() || groupId == 0){
+                _log.error(object);
+                return Response.status(Response.Status.BAD_REQUEST).entity("{\"error\":\"Bad Request: Estão vázios alguns dos campos (name, groupId).\"}").build();
+            }
+
+            // Busca todos os artigos no grupo
+            List<JournalArticle> articles = JournalArticleLocalServiceUtil.getArticles(groupId);
+
+            // Itera sobre todos os artigos e filtra pelo título
+            for (JournalArticle art : articles) {
+
+                if (art.getTitle().equals(name)) {
+                    pontJornalArticle = art;
+                    break;
+                }
+            }
+
+            if(pontJornalArticle == null){
+                return Response.status(Response.Status.NOT_FOUND)
+                        .entity("{\"error\":\"Not Found: Conteúdo web não encontrado.\"}").build();
+            }
+
+            String nomeArquivo = pontJornalArticle.getFolder().getName();
+
+            arquivo = String.valueOf(object.put("arquivo",nomeArquivo));
+
+        }catch (Exception e){
+            _log.error(e);
+            return Response.status(Response.Status.BAD_REQUEST).entity("{\"error\":\"Internal Server Error\"}"+e.toString()).build();
+        }
+
+        return Response.ok(object.toString(), MediaType.APPLICATION_JSON).build();
+    }
+
+    private final Log _log = LogFactoryUtil.getLog(EmailDinamiicRestServiceApplication.class);
+
+}
